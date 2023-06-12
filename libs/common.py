e7c7f4906cfb85bc08066c3c6fa5c8b0ccd703b2
morenod <dsanzmor@redhat.com>
2023-06-12 14:06:16 +0200

    Refactor subprocess execution and add colorlogs

--- libs/common.py
+++ libs/common.py
@@ -5,6 +5,7 @@ import os
 import errno
 import string
 import random
+import subprocess
 
 _es_ignored_metadata = "before-suite-metrics,route-latencies,route-throughputs,route-availabilities,healthchecks,healthcheckIteration,status"
 
@@ -120,3 +121,63 @@ def _generate_cluster_name_seed(cluster_name_seed):
         _cluster_name_seed = _cluster_name_seed[:6]
     _cluster_name_seed += "-" + random_string
     return _cluster_name_seed
+
+
+def _subprocess_exec(command, output_file=None, extra_params={}, log_output=True):
+    '''
+    Function to execute commands on a shell.
+    command: command to execute to be passed to subprocess. For example: "ls -l"
+    output_file: if defined, file to store output of the command. It will turn return values to None
+    extra_params: if defined, any extra param to be passed to Popen function in a mapping format. For example: extra_params={'cwd': '/tmp', 'universal_newlines': False}
+    log_output: if false, it wont print ERROR logs. useful when looping over commands which fails until they work, like oc login or oc adm
+
+    Function call example: exit_code, out, err = common._subprocess_exec("ls -l", extra_params={'cwd': '/tmp', 'universal_newlines': False}, log_output=False)
+    '''
+    logging.debug(command)
+    stdout = None
+    stderr = None
+    try:
+        log_file = open(output_file, 'w') if output_file else subprocess.PIPE
+        process = subprocess.Popen(command.split(), stdout=log_file, stderr=log_file, **extra_params)
+        stdout, stderr = process.communicate()
+        if process.returncode != 0 and log_output:
+            logging.error(f'Failed to execute command: {command}')
+            logging.error(stdout if stdout else "")
+            logging.error(stderr if stderr else "")
+        return process.returncode, stdout, stderr
+    except Exception as e:
+        logging.error(f'Error executing command: {command}')
+        logging.error(str(e))
+        logging.error(stdout if stdout else "")
+        logging.error(stderr if stderr else "")
+        return -1, None, None
+
+
+class CustomFormatter(logging.Formatter):
+    grey = '\x1b[38;21m'
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+    dark_green = '\x1b[38;5;22m'
+    light_green = '\x1b[38;5;46m'
+    dull_green = '\x1b[38;5;40m'
+    green = '\x1b[38;5;45m'
+    light_blue = '\x1b[38;5;117m'
+    reset = '\x1b[0m'
+
+    def __init__(self, fmt):
+        super().__init__()
+        self.fmt = fmt
+        self.FORMATS = {
+            logging.DEBUG: self.light_blue + self.fmt + self.reset,
+            logging.INFO: self.dull_green + self.fmt + self.reset,
+            logging.WARNING: self.yellow + self.fmt + self.reset,
+            logging.ERROR: self.red + self.fmt + self.reset,
+            logging.CRITICAL: self.bold_red + self.fmt + self.reset
+        }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
